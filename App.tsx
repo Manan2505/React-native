ed9d6f39e85ff8b2c9bf5b95cdf9b46242e0ec87
Manan Arora <arora.manan.k1234@gmail.com>
2025-04-15 12:25:47 +0530

    learned about synthetic events and button types on 15 april

--- App.tsx
+++ App.tsx
@@ -1,131 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+import {StyleSheet, View,Text, StatusBar, SafeAreaView,Image,Button, Alert, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from "react-native";
+import { useColorScheme} from "react-native";
+function App(){
+  const alertFunc=()=>{
+    // alert("button clicked");
+    console.log("HELLO")
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <SafeAreaView style={styles.statusbarMarginWithFlex}>
+      <TouchableHighlight onPress={alertFunc}>
+      {/* <View><Text>Highlight:</Text></View> */}
+        <View style={styles.btnStyling}>
+        <Text>Press Me</Text>
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      </TouchableHighlight>
+
+      <TouchableOpacity onPress={alertFunc}>
+        {/* <Text>Opacity:</Text> */}
+        <View style={styles.btnStyling}>
+        <Text>Press Me</Text>
         </View>
-      </ScrollView>
-    </View>
-  );
+      </TouchableOpacity>
+
+      <TouchableNativeFeedback onPress={alertFunc}>
+      {/* <Text>With Feedback:</Text> */}
+        <View style={styles.btnStyling}>
+        <Text>Press Me</Text>
+        </View>
+      </TouchableNativeFeedback>
+
+      <TouchableWithoutFeedback onPress={alertFunc}>
+      {/* <Text>Without feedback:</Text> */}
+        <View style={styles.btnStyling}>
+        <Text>Press Me</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+const styles=StyleSheet.create({
+  statusbarMarginWithFlex:{
+    marginTop:StatusBar.currentHeight,
+    borderRadius:'10',
+     alignItems:'center',
+    justifyContent:'center',
+    gap:'20'
+  },
+  btnStyling:{
+    width:'200',
+    height:'50',
+    backgroundColor:'red',
+   
+  }
+  
+});
