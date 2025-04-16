e1864307281f3a643266bad8700f503515445f29
Manan Arora <arora.manan.k1234@gmail.com>
2025-04-16 12:54:38 +0530

    learned about text input on 16-04-2025

--- App.tsx
+++ App.tsx
@@ -1,57 +1,38 @@
-import {StyleSheet, View,Text, StatusBar, SafeAreaView,Image,Button, Alert, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from "react-native";
-import { useColorScheme} from "react-native";
-function App(){
-  const alertFunc=()=>{
-    // alert("button clicked");
-    console.log("HELLO")
-  }
+import React from 'react'
+import { TextInput,StyleSheet,View, useColorScheme, } from 'react-native'
+
+function App() {
+  console.log(useColorScheme())
   return (
-    <SafeAreaView style={styles.statusbarMarginWithFlex}>
-      <TouchableHighlight onPress={alertFunc}>
-      {/* <View><Text>Highlight:</Text></View> */}
-        <View style={styles.btnStyling}>
-        <Text>Press Me</Text>
-        </View>
-      </TouchableHighlight>
+    
+      <View>
+        <TextInput style={styles.textCSS} 
+        placeholder="enter your name" 
+        onChangeText={(e)=>{console.log(e)}}
+        autoCapitalize='characters' 
+        // value='manan'
+        multiline={true}
+        numberOfLines={4}
+        keyboardType='numeric'
 
-      <TouchableOpacity onPress={alertFunc}>
-        {/* <Text>Opacity:</Text> */}
-        <View style={styles.btnStyling}>
-        <Text>Press Me</Text>
-        </View>
-      </TouchableOpacity>
 
-      <TouchableNativeFeedback onPress={alertFunc}>
-      {/* <Text>With Feedback:</Text> */}
-        <View style={styles.btnStyling}>
-        <Text>Press Me</Text>
-        </View>
-      </TouchableNativeFeedback>
-
-      <TouchableWithoutFeedback onPress={alertFunc}>
-      {/* <Text>Without feedback:</Text> */}
-        <View style={styles.btnStyling}>
-        <Text>Press Me</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    </SafeAreaView>
+         />
+      </View>
+   
   )
 }
-export default App;
+
+
+export default App
 
 const styles=StyleSheet.create({
-  statusbarMarginWithFlex:{
-    marginTop:StatusBar.currentHeight,
-    borderRadius:'10',
-     alignItems:'center',
-    justifyContent:'center',
-    gap:'20'
-  },
-  btnStyling:{
-    width:'200',
-    height:'50',
-    backgroundColor:'red',
-   
+  textCSS:{
+    margin:50,
+    borderWidth:1,
+    borderColor:'red',
+    borderRadius:10,
+    
   }
-  
-});
+ 
+
+})
